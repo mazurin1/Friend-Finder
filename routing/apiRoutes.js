bdffc13442c06aced0fd3e1eee61d1ecbdbaacd3
Marian <maazurin@gmail.com>
2019-07-31 22:21:59 -0700

    add scores and compare results and match

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,6 +9,28 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         //   * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
         //This route will also be used to handle the compatibility logic.
+        //for each friend in friends array
+        var bestMatch = {};
+        var bestScore = null;
+        for (var i = 0; i < friends.length; i++) {
+            if (bestScore == null) {
+                bestMatch = friends[i];
+                bestScore = 50;
+            }
+
+            var currentScore = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                currentScore = currentScore + Math.abs(friends[i].scores[j] - req.body.scores[j]);
+            }
+             
+            if (currentScore < bestScore) {
+                bestScore = currentScore;
+                bestMatch = friends[i];
+            }
+        }
         friends.push(req.body);
+        return res.json(bestMatch);
+
+
     });
 };
